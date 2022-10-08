928cbc328264249ef8380b331669b7caa722b059
TracyZhu0608 <yz5698@nyu.edu>
2022-10-07 21:29:11 -0400

    Revert "Adding data type details endpoint; test passing"
    This reverts commit 4f14d21495387e6014dae0697a5f651b5bf1e39c.

--- server/endpoints.py
+++ server/endpoints.py
@@ -12,15 +12,11 @@ api = Api(app)
 
 LIST = 'list'
 HELLO = '/hello'
-DETAILS = 'details'
 MESSAGE = 'message'
-DATA_LIST = f'/data_types/{LIST}'
-DATA_LIST_NM = 'data_types_list'
-DATA_TYPE_DETAILS = f'/data_types/{DETAILS}'
+DATA_LIST = f'/data_list/{LIST}'
+DATA_LIST_NM = 'data_list'
 
 A_DATA_TYPE = 'User'
-ANOTHER_DATA_TYPE = 'Project'
-
 
 
 @api.route('/hello')
@@ -46,7 +42,7 @@ class DataList(Resource):
         """
         Return a list of data names
         """
-        return {DATA_LIST_NM: ["A_DATA_TYPE", "ANOTHER_DATA_TYPE"]}
+        return {DATA_LIST_NM: ["A_DATA_TYPE"]}
 
 
 @api.route('/endpoints')
@@ -62,14 +58,3 @@ class Endpoints(Resource):
         endpoints = ''
         # sorted(rule.rule for rule in api.app.url_map.iter_rules())
         return {"Available endpoints": endpoints}
-
-@api.route(f'{DATA_TYPE_DETAILS}/<data_type>')
-class DataTypeDetails(Resource):
-    """
-    This will get a list of data types.
-    """
-    def get(self, data_type):
-        """
-        Returns a list of data types.
-        """
-        return {data_type: {}}
