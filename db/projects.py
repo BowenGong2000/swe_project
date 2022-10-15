bfa45b67f91b998fa487d145927b14b1493a9896
GeorgeHuang1126 <rh3019@nyu.edu>
2022-10-15 18:12:13 -0400

    indent fix

--- db/projects.py
+++ db/projects.py
@@ -7,10 +7,11 @@ NUM_MEMBERS = 'num_members'
 
 # We expect the project database to change frequently:
 projects = {TEST_PROJECT_NAME:
-            {NUM_MEMBERS: 7, 'Skill_requirement': 'math'},
-            'project2': {NUM_MEMBERS: 9, 
+            {NUM_MEMBERS: 7,
+            'Skill_requirement': 'math'},
+            'project2': {NUM_MEMBERS: 9,
             'Skill_requirement': 'computer_science'},
-            'project3': {NUM_MEMBERS: 6, 
+            'project3': {NUM_MEMBERS: 6,
             'Skill_requirement': 'design'},
             }
 
