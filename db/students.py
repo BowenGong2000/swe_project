24c0bd591235e9ef6767fba967e7cc0e9ccc3a50
GeorgeHuang1126 <rh3019@nyu.edu>
2022-10-21 21:00:24 -0400

    add functions add_student and student_exist

--- db/students.py
+++ db/students.py
@@ -27,6 +27,24 @@ def get_student_details(student):
     return students.get(student, None)
 
 
+def student_exists(name):
+    """
+    Returns whether or not the student exists.
+    """
+    return name in students
+
+
+def add_student(name, details):
+    if not isinstance(name, str):
+        raise TypeError(f'Wrong type for name: {type(name)=}')
+    if not isinstance(details, dict):
+        raise TypeError(f'Wrong type for details: {type(details)=}')
+    for field in REQUIRED_FLDS:
+        if field not in details:
+            raise ValueError(f'Required {field=} missing from details.')
+    students[name] = details
+
+
 def main():
     students = get_students()
     print(f'{students=}')
--- db/tests/test_students.py
+++ db/tests/test_students.py
@@ -12,3 +12,11 @@ def test_get_students():
 def test_get_student_details():
     std_dets = std.get_student_details(std.TEST_STUDENT_NAME)
     assert isinstance(std_dets, dict)
+
+
+def test_add_student():
+    details = {}
+    for field in std.REQUIRED_FLDS:
+        details[field] = 2
+    std.add_student(std.TEST_STUDENT_NAME, details)
+    assert std.student_exists(std.TEST_STUDENT_NAME)
