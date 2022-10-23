5324aa96f9e90e9c1ca2493568be08f22b474aa1
FantasiA10 <92002690+FantasiA10@users.noreply.github.com>
2022-10-23 15:35:05 -0400

    Update test_account.py

--- server/tests/test_account.py
+++ server/tests/test_account.py
@@ -7,7 +7,7 @@ def test_User_Validation():
     Check if user info are valid
     """
     test_user = {
-    "name": "Mark",
+    "name": "Mark123",
     "email": "yw4111@nyu.edu",
     "phone": "6467248912",
     "password": "Mark0445!"
