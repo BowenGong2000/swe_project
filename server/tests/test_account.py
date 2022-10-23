093ffe20d6b0da3a7e4cb9ce06d097a1936c1810
FantasiA10 <92002690+FantasiA10@users.noreply.github.com>
2022-10-23 14:57:57 -0400

    test update

--- server/tests/test_account.py
+++ server/tests/test_account.py
@@ -4,17 +4,15 @@ import pytest
 import server.models as md
 
 
-User = {
-    "name": "Mark",
-    "email": "yw4111@nyu.edu",
-    "phone": "6467248912",
-    "password": "Mark0445!"
-}
-
-
 def test_User_Validation():
     """
     Check if user info are valid
     """
-    test_res = md.User_Validation(User)
-    assert test_res == True
+    test_user = {
+    "name": "Mark",
+    "email": "yw4111@nyu.edu",
+    "phone": "6467248912",
+    "password": "Mark0445!"
+    }
+    testres = md.User_Validation(test_user)
+    assert testres
