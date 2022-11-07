cafbc1bfe26fe5ede31ea074b1933789fe8496ae
TracyZhu0608 <yz5698@nyu.edu>
2022-11-06 22:08:04 -0500

    format fix

--- server/test_app.py
+++ server/test_app.py
@@ -5,7 +5,8 @@ import db.projects as pj
 
 TEST_CLIENT = app.test_client()
 TEST_PROJECT_NAME = 'Test project'
-TEST_PROJECT  = pj.projects[TEST_PROJECT_NAME]
+TEST_PROJECT = pj.projects[TEST_PROJECT_NAME]
+
 
 def test_homepage():
     """
@@ -18,11 +19,12 @@ def test_homepage():
     response_post = TEST_CLIENT.post('/')
     assert response_post.status_code == 405
 
+
 def test_add_project():
     """
     when the '/add_project' page is requested (GET),
     see if the response is valid
-    """ 
+    """
     response_get = TEST_CLIENT.get('/add_project')
     assert response_get.status_code == 200
     assert pj.check_if_exist(TEST_PROJECT_NAME)
