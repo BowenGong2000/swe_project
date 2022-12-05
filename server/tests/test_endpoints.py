47d3fd805298eb1940a039fee6705a16afa3425c
GeorgeHuang1126 <rh3019@nyu.edu>
2022-12-04 20:49:50 -0500

    test add student

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -7,13 +7,7 @@ import db.students as std
 import db.sponsors as sps
 
 TEST_CLIENT = ep.app.test_client()
-TEST_DATA_TYPE = 'Project'
-
-TEST_PROJECT_NAME = 'Test project'
-TEST_PROJECT  = pj.projects[TEST_PROJECT_NAME]
-
-TEST_STUDENT_NAME = 'Test student'
-TEST_STUDENT = std.students[TEST_STUDENT_NAME]
+TEST_DATA_TYPE = 'Student'
 
 
 def test_hello():
@@ -23,66 +17,27 @@ def test_hello():
     resp_json = TEST_CLIENT.get(ep.HELLO).get_json()
     assert isinstance(resp_json[ep.MESSAGE], str)
 
-# def test_add_project():
-#     """
-#     see if adding project works properly.
-#     """
-#     resp = TEST_CLIENT.post(ep.PROJECT_ADD, json=TEST_PROJECT)
-#     assert pj.check_if_exist(TEST_PROJECT_NAME)
-#     pj.del_project(TEST_PROJECT_NAME)
 
-def test_get_projects_dict():
-    """
-    see if we can get project properly in a dictionary
-    """
-    projects = pj.get_projects_dict()
-    assert isinstance(projects, dict)
-    assert len(projects) > 0
+SAMPLE_STUDENT_NM = 'SampleStudent'
+SAMPLE_STUDENT = {
+    std.TEST_STUDENT_NAME: SAMPLE_STUDENT_NM,
+    std.EMAIL: 'x@y.com',
+    std.FULL_NAME: 'Sample Student',
+}
 
-def test_get_ProjectList(): 
-    """
-    see if we can get project list properly
-    should be store in a list and return a list
-    """
-    resp_json = TEST_CLIENT.get(ep.PROJECT_LIST_W_NS).get_json()
-    assert isinstance(resp_json[ep.PROJECT_LIST_NM], list) 
 
-def test_get_ProjectList_not_empty():
+def test_add_student():
     """
-    see if the project list has something in it
+    Test adding a student.
     """
-    resp_json = TEST_CLIENT.get(ep.PROJECT_LIST_W_NS).get_json()
-    assert len(resp_json[ep.PROJECT_LIST_NM]) > 0
+    resp = TEST_CLIENT.post(ep.STUDENT_ADD, json=SAMPLE_STUDENT)
+    assert std.student_exists(SAMPLE_STUDENT_NM)
+    std.del_user(SAMPLE_STUDENT_NM)
 
-def test_get_project_details():
-    """
-    see if we can get project details properly
-    """
-    resp_json = TEST_CLIENT.get(f'{ep.PROJECT_DETAILS_W_NS}/{TEST_PROJECT}').get_json()
-    assert isinstance(resp_json, dict)
-
-def test_get_students_dict():
-    """
-    see if we can get students properly in a dictionary
-    """
-    students = std.get_students_dict()
-    assert isinstance(students, dict)
-    assert len(students) > 0
-
-def test_get_student_details():
-    """
-    see if we can get student details properly
-    """
-    resp_json = TEST_CLIENT.get(f'{ep.STUDENT_DETAILS_W_NS}/{TEST_STUDENT}').get_json()
-    assert isinstance(resp_json, dict)
-
-def test_get_sponsors_dict():
-    """
-    see if we can get sponsors properly in a dictionary
-    """
-    sponsors = sps.get_sponsors_dict()
-    assert isinstance(sponsors, dict)
-    assert len(sponsors) > 0
+def test_get_students_list():
+    resp = TEST_CLIENT.get(ep.STUDENT_LIST_W_NS)
+    resp_json = resp.get_json()
+    assert isinstance(resp_json[ep.USER_LIST_NM], list)
 
 def test_get_DataList(): 
     """
