19d4e6289a8054298a83a3b9b2632bbd5ee987df
BowenGong1125 <67953712+BowenGong2000@users.noreply.github.com>
2023-03-05 22:53:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -183,16 +183,5 @@ def about_us():
 def contact_us():
   return render_template("contact.html")
 
-# @app.route('/upload')
-# def upload_file():
-#    return render_template('upload.html')
-
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
-
 if __name__ == '__main__':    
     app.run(debug=True)
